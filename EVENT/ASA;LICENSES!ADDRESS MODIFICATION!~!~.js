d6d10d32986e683e24ed4a28303e9c2d9e4344c6
sshiralikar <110970408+sshiralikar@users.noreply.github.com>
2022-08-10 14:13:04 +0530

    Update ASA;LICENSES!ADDRESS MODIFICATION!~!~.js
    Initial

--- EVENT/ASA;LICENSES!ADDRESS MODIFICATION!~!~.js
+++ EVENT/ASA;LICENSES!ADDRESS MODIFICATION!~!~.js
@@ -1,1 +1,11 @@
-
+//Application submit after actions for amending address
+parentName = null ; 
+parentCapId = getParent();
+if(parentCapId)
+{
+	 parentCap = aa.cap.getCap(parentCapId).getOutput();  
+	 if (parentCap) 
+	 	parentName = parentCap.getSpecialText();
+}
+if(parentName)
+	editAppName(parentName);
