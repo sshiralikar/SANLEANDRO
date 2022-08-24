305aee1d27524851832a9464df3c24df3059a97e
sshiralikar <110970408+sshiralikar@users.noreply.github.com>
2022-08-24 09:23:02 +0530

    Create ENF CREATE CHILD CASES.js

---
+++ SCRIPTS/ENF CREATE CHILD CASES.js
@@ -0,0 +1,11 @@
+newAppId = createChild(newAppL1,newAppL2,newAppL3,newAppL4,newAppDesc);
+if(newAppId)
+{
+    copyAppSpecific(newAppId);
+    updateAppStatus("Investigation Pending","Updated by Script",newAppId);
+    holdCapId = capId ; capId = newAppId; closeTask("Case Intake","Assigned","",""); 
+    scheduleInspectDate("Initial Investigation",dateAdd(null,1,true)); 
+    capId = holdCapId;
+    aa.cap.copyCapWorkDesInfo(capId, newAppId);
+    editPriority(AInfo["Priority"],newAppId);
+}
