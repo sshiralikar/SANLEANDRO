00633250df52036555bb58e2d884e999914a5d38
sshiralikar <sshiralikar@trustvip.com>
2022-10-14 12:27:44 -0400

    CASANLEAN-1129

--- EVENT/WTUA;BUILDING!RESIDENTIAL!ALTERATION!NA.js
+++ EVENT/WTUA;BUILDING!RESIDENTIAL!ALTERATION!NA.js
@@ -189,6 +189,63 @@ if(wfTask == "Plans Coordination" && wfStatus == "Approved - Fee Due")
     sendEmail("", applicantEmail, "", "BLD_APPROVED_FEES_DUE", params, null, capId);
 }
 //CASANLEAN-1140
+//CASANLEAN-1129
+if(wfTask == "Application Intake" && wfStatus == "Additional Info Required")
+{
+    var params = aa.util.newHashtable();
+
+    var vStaffId = "";
+    var title = "";
+    var vEmail = "";
+    var wfUserName = "";
+    var currentUsrVar = aa.person.getUser(wfStaffUserID).getOutput();
+    if (currentUsrVar != null) {
+        vStaffId = currentUsrVar.getGaUserID();
+        title = currentUsrVar.title;
+        vEmail = currentUsrVar.email;
+        wfUserName = currentUsrVar.firstName + " "+ currentUsrVar.middleName+ " "+ currentUsrVar.lastName;
+    }
+
+    var vAddress = "";
+    var capAddressResult1 = aa.address.getAddressByCapId(capId);
+    if (capAddressResult1.getSuccess())
+    {
+        var Address = capAddressResult1.getOutput();
+        for (yy in Address)
+        {
+            vAddress = Address[yy].getHouseNumberStart();
+            if (Address[yy].getStreetDirection())
+                vAddress += " " + Address[yy].getStreetDirection();
+            vAddress += " " + Address[yy].getStreetName();
+            if (Address[yy].getStreetSuffix())
+                vAddress += " " + Address[yy].getStreetSuffix();
+            if (Address[yy].getUnitStart())
+                vAddress += " " + Address[yy].getUnitStart();
+        }
+    }
+    var applicantEmail = "";
+    var conName = "";
+    var contactResult = aa.people.getCapContactByCapID(capId);
+    if (contactResult.getSuccess()) {
+        var capContacts = contactResult.getOutput();
+        for (var i in capContacts) {
+            if (capContacts[i].getPeople().getContactType() == "Applicant") {
+                conName = getContactName(capContacts[i]);
+                applicantEmail = capContacts[i].getPeople().getEmail()+"";
+            }
+        }
+    }
+    addParameter(params, "$$applicantName$$", conName);
+    addParameter(params, "$$altID$$", capId.getCustomID()+"");
+    addParameter(params, "$$projectDescription$$", cap.getSpecialText());
+    addParameter(params, "$$Location$$", vAddress);
+    addParameter(params, "$$assignedToStaff$$", wfUserName);
+    addParameter(params, "$$assignedUserTitle$$", title);
+    addParameter(params, "$$assignedUserEmail$$", vEmail);
+    addParameter(params, "$$wfTaskComments$$", wfComment);
+    sendEmail("", applicantEmail, "", "BLD_ADDITIONAL_INFO_REQ", params, null, capId);
+}
+//CASANLEAN-1129
 
 //CASANLEAN-1136
 if(wfTask == "Fire Review" && (wfStatus == "Approved" || wfStatus == "Approved w/ Comments"))
@@ -214,6 +271,12 @@ if(wfTask == "Fire Review" && (wfStatus == "Approved" || wfStatus == "Approved w
         addFee("MISC","B_FIRE","FINAL",feeAmt * 0.65,"N");
 }
 //CASANLEAN-1136
+//CASANLEAN-1135
+if(wfTask == "Planning Review" && (wfStatus == "Approved" || wfStatus == "Approved w/ Comments"))
+{
+    addFee("PLNNRR","P_PLN","FINAL",1,"N");
+}
+//CASANLEAN-1135
 
 function sendEmail(fromEmail, toEmail, CC, template, eParams, files) { // optional: itemCap
     var itemCap = capId;
