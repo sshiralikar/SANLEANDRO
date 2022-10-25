c48f520ba06e6d7fac988ab0a5e753eeda18a0bf
sshiralikar <sshiralikar@trustvip.com>
2022-10-25 15:22:32 -0400

    CASANLEAN-593

--- EVENT/WTUA;BUILDING!COMMERCIAL!ALTERATION!NA.js
+++ EVENT/WTUA;BUILDING!COMMERCIAL!ALTERATION!NA.js
@@ -76,6 +76,64 @@ if(wfTask == "Plans Coordination" && wfStatus == "Resubmittal Required")
 }
 //CASANLEAN-1142
 
+//CASALEAN-593
+if(wfTask == "Plans Distribution" && wfStatus == "Resubmittal Required")
+{
+    var params = aa.util.newHashtable();
+
+    var vStaffId = "";
+    var title = "";
+    var vEmail = "";
+    var wfUserName = "";
+    var currentUsrVar = aa.person.getUser(wfStaffUserID).getOutput();
+    if (currentUsrVar != null) {
+        vStaffId = currentUsrVar.getGaUserID();
+        title = currentUsrVar.title;
+        vEmail = currentUsrVar.email;
+        wfUserName = currentUsrVar.firstName + " "+ currentUsrVar.middleName+ " "+ currentUsrVar.lastName;
+    }
+
+    var vAddress = "";
+    var capAddressResult1 = aa.address.getAddressByCapId(capId);
+    if (capAddressResult1.getSuccess())
+    {
+        var Address = capAddressResult1.getOutput();
+        for (yy in Address)
+        {
+            vAddress = Address[yy].getHouseNumberStart();
+            if (Address[yy].getStreetDirection())
+                vAddress += " " + Address[yy].getStreetDirection();
+            vAddress += " " + Address[yy].getStreetName();
+            if (Address[yy].getStreetSuffix())
+                vAddress += " " + Address[yy].getStreetSuffix();
+            if (Address[yy].getUnitStart())
+                vAddress += " " + Address[yy].getUnitStart();
+        }
+    }
+    var applicantEmail = "";
+    var conName = "";
+    var contactResult = aa.people.getCapContactByCapID(capId);
+    if (contactResult.getSuccess()) {
+        var capContacts = contactResult.getOutput();
+        for (var i in capContacts) {
+            if (capContacts[i].getPeople().getContactType() == "Applicant") {
+                conName = getContactName(capContacts[i]);
+                applicantEmail = capContacts[i].getPeople().getEmail()+"";
+            }
+        }
+    }
+    addParameter(params, "$$applicantName$$", conName);
+    addParameter(params, "$$altID$$", capId.getCustomID()+"");
+    addParameter(params, "$$projectDescription$$", cap.getSpecialText());
+    addParameter(params, "$$Location$$", vAddress);
+    addParameter(params, "$$assignedToStaff$$", wfUserName);
+    addParameter(params, "$$assignedUserTitle$$", title);
+    addParameter(params, "$$assignedUserEmail$$", vEmail);
+    sendEmail("", applicantEmail, "", "BLD_PLANCHECKCOMMENTS", params, null, capId);
+}
+//CASALEAN-593
+
+
 //CASANLEAN-1141
 if(wfTask == "Plans Coordination" && wfStatus == "Hold for Hard Copies")
 {
