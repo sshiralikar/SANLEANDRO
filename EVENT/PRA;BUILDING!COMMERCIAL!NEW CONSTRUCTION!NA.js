839291f6b85d1d6a0beffbd993947b5cb659aeed
ffx\xsshira <sshiralikar@trustvip.com>
2022-11-02 21:48:42 -0400

    CASANLEAN-960

--- EVENT/PRA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
+++ EVENT/PRA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
@@ -2,3 +2,14 @@
 if(publicUser && isTaskActive("Plans Distribution") && balanceDue == 0)
     updateTask("Plans Distribution","Fees Paid","","");
 //CASANLEAN-960
+//CASANLEAN-963
+if((wfTask == "Planning Review" || wfTask == "Public Works Review"
+        || wfTask == "Environmental Services Review" || wfTask == "Engineering & Transportation")
+    && (wfStatus == "Approved" || wfStatus == "Approved w/ Comments")
+    && (wfHours == null || wfHours == ""))
+{
+    cancel = true;
+    showMessage = true;
+    comment("Please enter the <b>Hours Spent</b>.");
+}
+//CASANLEAN-963
