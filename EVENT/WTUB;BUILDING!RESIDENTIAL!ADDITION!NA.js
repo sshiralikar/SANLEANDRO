36860d09c4af25e00c0b333bfbd4dc476f8e6f6c
ffx\xsshira <sshiralikar@trustvip.com>
2022-11-03 00:54:09 -0400

    WTUA BRADD

---
+++ EVENT/WTUB;BUILDING!RESIDENTIAL!ADDITION!NA.js
@@ -0,0 +1,15 @@
+if(wfTask == "Plans Coordination" && wfStatus == "Approved - Fee Due" && balanceDue <= 0)
+{
+    cancel = true;
+    showMessage = true;
+    comment("The fee balance is <b>Zero</b>.");
+}
+if((wfTask == "Planning Review" || wfTask == "Public Works Review"
+        || wfTask == "Environmental Services Review" || wfTask == "Engineering & Transportation")
+    && (wfStatus == "Approved" || wfStatus == "Approved w/ Comments")
+    && (wfHours == null || wfHours == ""))
+{
+    cancel = true;
+    showMessage = true;
+    comment("Please enter the <b>Hours Spent</b>.");
+}
