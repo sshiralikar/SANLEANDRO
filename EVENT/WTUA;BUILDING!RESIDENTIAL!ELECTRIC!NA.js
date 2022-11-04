abed794202672f92e2e5ee3710675fd14b929901
ffx\xsshira <sshiralikar@trustvip.com>
2022-11-04 12:50:47 -0400

    Month +1

--- EVENT/WTUA;BUILDING!RESIDENTIAL!ELECTRIC!NA.js
+++ EVENT/WTUA;BUILDING!RESIDENTIAL!ELECTRIC!NA.js
@@ -5,7 +5,7 @@ if(wfTask == "Application Intake" && (wfStatus == "Accepted - Plan Review Not Re
     var year = d.getFullYear();
     var month = d.getMonth();
     var day = d.getDate();
-    var c = new Date(year+1, month, day);
+    var c = new Date(year+1, month+1, day);
     var newDate = c.getMonth()+"/"+c.getDate()+"/"+c.getFullYear();
     editAppSpecific("Application Expiration Date", newDate);
 }
@@ -17,7 +17,7 @@ if(wfTask == "Permit Issuance" && wfStatus == "Issued")
     var year = d.getFullYear();
     var month = d.getMonth();
     var day = d.getDate();
-    var c = new Date(year+1, month, day);
+    var c = new Date(year+1, month+1, day);
     var newDate = c.getMonth()+"/"+c.getDate()+"/"+c.getFullYear();
     editAppSpecific("Permit Expiration Date", newDate);
     editAppSpecific("Permit Issued Date", (d.getMonth()+1)+"/"+d.getDate()+"/"+d.getFullYear());
