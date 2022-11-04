f0869c865181a816f3b08d729af7f163a29afe0f
ffx\xsshira <sshiralikar@trustvip.com>
2022-11-03 23:14:49 -0400

    WTUB BRELEC

---
+++ EVENT/WTUB;BUILDING!RESIDENTIAL!ELECTRIC!NA.js
@@ -0,0 +1,19 @@
+//CASANLEAN-448
+if(wfTask == "Plans Coordination" && wfStatus == "Approved - Fee Due" && balanceDue <= 0)
+{
+    cancel = true;
+    showMessage = true;
+    comment("The fee balance is <b>Zero</b>.");
+}
+//CASANLEAN-448
+//CASANLEAN-926
+if((wfTask == "Planning Review" || wfTask == "Public Works Review"
+        || wfTask == "Environmental Services Review" || wfTask == "Engineering & Transportation")
+    && (wfStatus == "Approved" || wfStatus == "Approved w/ Comments")
+    && (wfHours == null || wfHours == ""))
+{
+    cancel = true;
+    showMessage = true;
+    comment("Please enter the <b>Hours Spent</b>.");
+}
+//CASANLEAN-926
