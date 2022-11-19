f1727446b5070996e31c527738f3a1d5fd814e97
ffx\xsshira <sshiralikar@trustvip.com>
2022-11-19 17:50:51 -0500

    Changes

--- EVENT/PRA;BUILDING!COMMERCIAL!ALTERATION!NA.js
+++ EVENT/PRA;BUILDING!COMMERCIAL!ALTERATION!NA.js
@@ -1,2 +1,5 @@
 if(publicUser && isTaskActive("Plans Distribution") && balanceDue == 0)
+{
     updateTask("Plans Distribution","Fees Paid","","");
+    updateAppStatus("In Review","Updated through script");
+}
--- EVENT/PRA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
+++ EVENT/PRA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
@@ -1,4 +1,7 @@
 //CASANLEAN-960
 if(publicUser && isTaskActive("Plans Distribution") && balanceDue == 0)
+{
     updateTask("Plans Distribution","Fees Paid","","");
+    updateAppStatus("In Review","Updated through script");
+}
 //CASANLEAN-960
