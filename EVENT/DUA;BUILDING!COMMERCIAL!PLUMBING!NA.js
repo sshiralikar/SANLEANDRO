2545c8d69fc2d29fe768e46b717fbde8e7180851
ffx\xsshira <sshiralikar@trustvip.com>
2022-11-20 20:41:07 -0500

    BCPLUMB - Initial

---
+++ EVENT/DUA;BUILDING!COMMERCIAL!PLUMBING!NA.js
@@ -0,0 +1,14 @@
+//CASANLEAN-254
+var workfHistory = aa.workflow.getWorkflowHistory(capId, null);
+if (workfHistory.getSuccess()) {
+    var wfhistoryresult = workfHistory.getOutput();
+}
+for (var i in wfhistoryresult) {
+    var pTask = wfhistoryresult[i];
+    if (pTask.getTaskDescription() == "Plans Distribution"
+        && pTask.getDisposition() == "Resubmittal Required") {
+        updateTask("Plans Distribution","Updated Documents","","");
+        break;
+    }
+}
+//CASANLEAN-254
