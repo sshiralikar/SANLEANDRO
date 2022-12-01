ace105c521d66a55f0799b6ac128c36163c06464
Shashank <sshiralikar@trustvip.com>
2022-12-01 11:48:51 -0500

    Adding PC for PRA

--- EVENT/PRA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
+++ EVENT/PRA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
@@ -1,7 +1,10 @@
-//CASANLEAN-960
 if(isTaskActive("Plans Distribution") && balanceDue == 0)
 {
     updateTask("Plans Distribution","Fees Paid","","");
     updateAppStatus("In Review","Updated through script");
 }
-//CASANLEAN-960
+else if(isTaskActive("Plans Coordination") && balanceDue == 0)
+{
+    updateTask("Plans Coordination","Fees Paid","","");
+    updateAppStatus("In Review","Updated through script");
+}
