6a4c741ccce15485932023c629a6923841903ab8
Shashank <sshiralikar@trustvip.com>
2022-12-01 11:38:05 -0500

    WTUA - YEar + 1 correction

--- EVENT/WTUA;BUILDING!RESIDENTIAL!MECHANICAL!NA.js
+++ EVENT/WTUA;BUILDING!RESIDENTIAL!MECHANICAL!NA.js
@@ -1,22 +1,20 @@
+//CASANLEAN-938
 if(wfTask == "Application Intake" && (wfStatus == "Accepted - Plan Review Not Req" || wfStatus == "Accepted - Plan Review Req"))
 {
-    var d = new Date();
-    var year = d.getFullYear();
-    var month = d.getMonth();
-    var day = d.getDate();
-    var c = new Date(year + 1, month+1, day);
-    var newDate = c.getMonth()+"/"+c.getDate()+"/"+c.getFullYear();
+    var c = new Date();
+    c.setFullYear(c.getFullYear() + 1);
+    var newDate = c.getMonth()+1+"/"+c.getDate()+"/"+c.getFullYear();
     editAppSpecific("Application Expiration Date", newDate);
 }
+//CASANLEAN-1194
+//CASANLEAN-1193
 if(wfTask == "Permit Issuance" && wfStatus == "Issued")
 {
-    var d = new Date();
-    var year = d.getFullYear();
-    var month = d.getMonth();
-    var day = d.getDate();
-    var c = new Date(year + 1, month+1, day);
-    var newDate = c.getMonth()+"/"+c.getDate()+"/"+c.getFullYear();
+    var c = new Date();
+    c.setFullYear(c.getFullYear() + 1);
+    var newDate = c.getMonth()+1+"/"+c.getDate()+"/"+c.getFullYear();
     editAppSpecific("Permit Expiration Date", newDate);
+    var d = new Date();
     editAppSpecific("Permit Issued Date", (d.getMonth()+1)+"/"+d.getDate()+"/"+d.getFullYear());
 }
 //CASANLEAN-444
