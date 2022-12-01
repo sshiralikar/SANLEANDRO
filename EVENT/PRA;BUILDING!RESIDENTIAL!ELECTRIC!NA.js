746e7e7ca7b9640d5a88b1f5d65877d2e6dc4ff3
Shashank <sshiralikar@trustvip.com>
2022-12-01 13:18:31 -0500

    Adding PC for PRA

--- EVENT/PRA;BUILDING!RESIDENTIAL!ELECTRIC!NA.js
+++ EVENT/PRA;BUILDING!RESIDENTIAL!ELECTRIC!NA.js
@@ -6,5 +6,7 @@ if(isTaskActive("Plans Distribution") && balanceDue == 0)
 else if(isTaskActive("Plans Coordination") && balanceDue == 0)
 {
     updateTask("Plans Coordination","Fees Paid","","");
+    activateTask("Permit Issuance");
+    deactivateTask("Plans Coordination");
     updateAppStatus("In Review","Updated through script");
 }
