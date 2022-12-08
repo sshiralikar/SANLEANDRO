f0081612c15d425f81cc3d38395fd6a22427540e
Shashank <sshiralikar@trustvip.com>
2022-12-08 12:08:16 -0500

    INITIAL - BCFIRE

--- EVENT/PRA;BUILDING!COMMERCIAL!FIRE!NA.js
+++ EVENT/PRA;BUILDING!COMMERCIAL!FIRE!NA.js
@@ -3,6 +3,11 @@ if(isTaskActive("Plans Distribution") && balanceDue == 0)
     updateTask("Plans Distribution","Fees Paid","","");
     updateAppStatus("In Review","Updated through script");
 }
+else if(isTaskActive("Fire Plan Review") && balanceDue == 0)
+{
+    updateTask("Plans Distribution","Fees Paid","","");
+    updateAppStatus("In Review","Updated through script");
+}
 else if(isTaskActive("Plans Coordination") && balanceDue == 0)
 {
     updateTask("Plans Coordination","Fees Paid","","");
