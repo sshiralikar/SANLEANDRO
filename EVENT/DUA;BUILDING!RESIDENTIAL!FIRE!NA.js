d86ab6bf11314083bc3da976fecf882a52d86439
Shashank <sshiralikar@trustvip.com>
2022-12-08 12:25:46 -0500

    INITIAL - BRFIRE

---
+++ EVENT/DUA;BUILDING!RESIDENTIAL!FIRE!NA.js
@@ -0,0 +1,17 @@
+var workfHistory = aa.workflow.getWorkflowHistory(capId, null);
+if (workfHistory.getSuccess()) {
+    var wfhistoryresult = workfHistory.getOutput();
+}
+for (var i in wfhistoryresult) {
+    var pTask = wfhistoryresult[i];
+    if (pTask.getTaskDescription() == "Plans Distribution"
+        && pTask.getDisposition() == "Resubmittal Required") {
+        updateTask("Plans Distribution","Updated Documents","","");
+        break;
+    }
+    if (pTask.getTaskDescription() == "Fire Plan Review"
+        && pTask.getDisposition() == "Resubmittal Required") {
+        updateTask("Plans Distribution","Updated Documents","","");
+        break;
+    }
+}
---
+++ EVENT/PRA;BUILDING!RESIDENTIAL!FIRE!NA.js
@@ -0,0 +1,17 @@
+if(isTaskActive("Plans Distribution") && balanceDue == 0)
+{
+    updateTask("Plans Distribution","Fees Paid","","");
+    updateAppStatus("In Review","Updated through script");
+}
+else if(isTaskActive("Fire Plan Review") && balanceDue == 0)
+{
+    updateTask("Plans Distribution","Fees Paid","","");
+    updateAppStatus("In Review","Updated through script");
+}
+else if(isTaskActive("Plans Coordination") && balanceDue == 0)
+{
+    updateTask("Plans Coordination","Fees Paid","","");
+    activateTask("Permit Issuance");
+    deactivateTask("Plans Coordination");
+    updateAppStatus("In Review","Updated through script");
+}
