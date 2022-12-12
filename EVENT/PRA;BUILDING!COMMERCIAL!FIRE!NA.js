a9da58f412e0b2bedf763de9fd1b456ca803c561
Shashank <sshiralikar@trustvip.com>
2022-12-12 14:59:20 -0500

    Correction on PRA Fire

--- EVENT/PRA;BUILDING!COMMERCIAL!FIRE!NA.js
+++ EVENT/PRA;BUILDING!COMMERCIAL!FIRE!NA.js
@@ -5,7 +5,7 @@ if(isTaskActive("Plans Distribution") && balanceDue == 0)
 }
 else if(isTaskActive("Fire Plan Review") && balanceDue == 0)
 {
-    updateTask("Plans Distribution","Fees Paid","","");
+    updateTask("Fire Plan Review","Fees Paid","","");
     updateAppStatus("In Review","Updated through script");
 }
 else if(isTaskActive("Plans Coordination") && balanceDue == 0)
