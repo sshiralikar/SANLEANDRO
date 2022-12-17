db73f7130b5c9a1323b7eb42f3f35cbe05f57b1f
Shashank <sshiralikar@trustvip.com>
2022-12-17 02:42:00 -0500

    Adding email filter

--- EVENT/WTUA;BUILDING!COMMERCIAL!FIRE!NA.js
+++ EVENT/WTUA;BUILDING!COMMERCIAL!FIRE!NA.js
@@ -470,7 +470,7 @@ function sendEmail(fromEmail, toEmail, CC, template, eParams, files) { // option
         itemCap = arguments[6]; // use cap ID specified in args
 
     //var sent = aa.document.sendEmailByTemplateName(fromEmail, toEmail, CC, template, eParams, files);
-    fromEmail = runEmailThroughSLEmailFilter(fromEmail);
+    toEmail = runEmailThroughSLEmailFilter(toEmail);
     var itempAltIDScriptModel = aa.cap.createCapIDScriptModel(itemCap.getID1(), itemCap.getID2(), itemCap.getID3());
     var sent = aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, CC, template, eParams, itempAltIDScriptModel, files);
     if (!sent.getSuccess()) {
