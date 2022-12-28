cc6ffea3f29e29f1f9136402007a9f255c404cb7
Shashank <sshiralikar@trustvip.com>
2022-12-28 10:58:33 -0500

    CASANLEAN-1478

--- EVENT/PRA;BUILDING!RESIDENTIAL!NEW CONSTRUCTION!NA.js
+++ EVENT/PRA;BUILDING!RESIDENTIAL!NEW CONSTRUCTION!NA.js
@@ -8,5 +8,6 @@ else if(isTaskActive("Plans Coordination") && balanceDue == 0)
     updateTask("Plans Coordination","Fees Paid","","");
     activateTask("Permit Issuance");
     deactivateTask("Plans Coordination");
+    aa.workflow.adjustTask(capId, "Plans Coordination", "N", "Y", null, null);
     updateAppStatus("In Review","Updated through script");
 }
