53579309c410b2cd728be3b42469102389775daa
Shashank <sshiralikar@trustvip.com>
2023-01-03 23:23:24 -0500

    CASANLEAN-1461

---
+++ EVENT/IRSA;BUILDING!~!~!~.js
@@ -0,0 +1,8 @@
+if(isTaskActive("Inspection"))
+{
+    updateTask("Inspection"," Final Inspection Complete","","");
+    //activateTask("Permit Issuance");
+    deactivateTask("Inspection");
+    aa.workflow.adjustTask(capId, "Inspection", "N", "Y", null, null);
+    updateAppStatus("Finaled","Updated through script");
+}
