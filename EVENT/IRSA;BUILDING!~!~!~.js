a8db09c21163f9ae227233b073461a33974b23f8
Shashank <sshiralikar@trustvip.com>
2023-01-11 18:18:46 -0500

    CASANLEAN-1385

--- EVENT/IRSA;BUILDING!~!~!~.js
+++ EVENT/IRSA;BUILDING!~!~!~.js
@@ -1,10 +1,54 @@
 showDebug = false;
 showMessage = false;
-if(isTaskActive("Inspection") && inspResult == "Finaled")
+if(isTaskActive("Inspection") && inspResult == "Finaled" && isAllConditionsMet(capId) && balanceDue <= 0)
 {
-    updateTask("Inspection"," Final Inspection Complete","","");
-    //activateTask("Permit Issuance");
-    deactivateTask("Inspection");
-    aa.workflow.adjustTask(capId, "Inspection", "N", "Y", null, null);
-    updateAppStatus("Finaled","Updated through script");
+    var flag = true;
+    var relChildren = getChildren("Building/*/*/*", capId);
+    if (!matches(relChildren, null, false)) {
+        for (var r in relChildren) {
+            var capDetailObjResult = aa.cap.getCapDetail(relChildren[r]);            // Detail
+            if (capDetailObjResult.getSuccess())
+            {
+                capDetail = capDetailObjResult.getOutput();
+                var vBalanceDue = capDetail.getBalance();
+                if(parseFloat(vBalanceDue) > 0)
+                    flag = false;
+            }
+        }
+    }
+    if(flag)
+    {
+        updateTask("Inspection"," Final Inspection Complete","","");
+        aa.workflow.adjustTask(capId, "Inspection", "N", "Y", null, null);
+        updateAppStatus("Finaled","Updated through script");
+
+        var relChildren = getChildren("Building/*/*/*", capId);
+        if (!matches(relChildren, null, false)) {
+            for (var r in relChildren) {
+                updateAppStatus("Finaled","Updated through script",relChildren[r]);
+                updateTask("Inspection"," Final Inspection Complete","","","",relChildren[r]);
+                aa.workflow.adjustTask(relChildren[r], "Inspection", "N", "Y", null, null);
+            }
+        }
+    }
+}
+
+function isAllConditionsMet(vCapId)
+{
+    var condResult = aa.capCondition.getCapConditions(vCapId);
+    if (condResult.getSuccess()) {
+        var capConds = condResult.getOutput();
+        for (var cc in capConds) {
+            var thisCondX = capConds[cc];
+            var cNbr = thisCondX.getConditionNumber();
+            var thisCond = aa.capCondition.getCapCondition(cIds[x],cNbr).getOutput();
+            var cStatus = thisCond.getConditionStatus();
+            //var isCOA = thisCond.getConditionOfApproval();
+            if(matches(cStatus, "Not Met", "Applied"))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
