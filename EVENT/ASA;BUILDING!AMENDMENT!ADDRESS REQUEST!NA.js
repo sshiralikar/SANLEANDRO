ab0f65c9c0303eac867a2920d230c54afaaec138
Shashank <sshiralikar@trustvip.com>
2023-01-18 14:06:18 -0500

    //CASANLEAN-1534

--- EVENT/ASA;BUILDING!AMENDMENT!ADDRESS REQUEST!NA.js
+++ EVENT/ASA;BUILDING!AMENDMENT!ADDRESS REQUEST!NA.js
@@ -4,5 +4,72 @@ if (vParentId != null && vParentId != false && vParentId != "undefined") {
     copyParcels(vParentId,capId);
     copyOwnersByParcel();
     copyAddresses(vParentId, capId);
-    updateWorkDesc(vParentId,capId);
+    //updateWorkDesc(vParentId,capId);
+    copyAdditionalInfo(vParentId,capId);
+    aa.cap.copyCapDetailInfo(vParentId,capId);
+    aa.cap.copyCapWorkDesInfo(vParentId,capId);
+}
+function copyAdditionalInfo(srcCapId, targetCapId)
+{
+    //1. Get Additional Information with source CAPID.  (BValuatnScriptModel)
+    var  additionalInfo = getAdditionalInfo(srcCapId);
+    if (additionalInfo == null)
+    {
+        return;
+    }
+    //2. Get CAP detail with source CAPID.
+    var  capDetail = getCapDetailByID(srcCapId);
+    //3. Set target CAP ID to additional info.
+    additionalInfo.setCapID(targetCapId);
+    if (capDetail != null)
+    {
+        capDetail.setCapID(targetCapId);
+    }
+    //4. Edit or create additional infor for target CAP.
+    aa.cap.editAddtInfo(capDetail, additionalInfo);
+}
+
+//Return BValuatnScriptModel for additional info.
+function getAdditionalInfo(capId)
+{
+    bvaluatnScriptModel = null;
+    var s_result = aa.cap.getBValuatn4AddtInfo(capId);
+    if(s_result.getSuccess())
+    {
+        bvaluatnScriptModel = s_result.getOutput();
+        if (bvaluatnScriptModel == null)
+        {
+            aa.print("WARNING: no additional info on this CAP:" + capId);
+            bvaluatnScriptModel = null;
+        }
+    }
+    else
+    {
+        aa.print("ERROR: Failed to get additional info: " + s_result.getErrorMessage());
+        bvaluatnScriptModel = null;
+    }
+    // Return bvaluatnScriptModel
+    return bvaluatnScriptModel;
+}
+
+function getCapDetailByID(capId)
+{
+    capDetailScriptModel = null;
+    var s_result = aa.cap.getCapDetail(capId);
+    if(s_result.getSuccess())
+    {
+        capDetailScriptModel = s_result.getOutput();
+        if (capDetailScriptModel == null)
+        {
+            aa.print("WARNING: no cap detail on this CAP:" + capId);
+            capDetailScriptModel = null;
+        }
+    }
+    else
+    {
+        aa.print("ERROR: Failed to get cap detail: " + s_result.getErrorMessage());
+        capDetailScriptModel = null;
+    }
+    // Return capDetailScriptModel
+    return capDetailScriptModel;
 }
