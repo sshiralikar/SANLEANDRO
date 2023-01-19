ed6929c0c8d7a7501ac68a24ac6173391d4bfdbf
Shashank <sshiralikar@trustvip.com>
2023-01-19 00:06:35 -0500

    //CASANLEAN-1388

--- EVENT/ASB;BUILDING!RESIDENTIAL!PLUMBING!NA.js
+++ EVENT/ASB;BUILDING!RESIDENTIAL!PLUMBING!NA.js
@@ -1,1 +1,29 @@
-// Enter your script here...
+try {
+    //CASANLEAN-1388
+    if(!isDistrictValid())
+    {
+        cancel = true;
+        showMessage = true;
+        comment("Your location is outside of our area. Please contact the Oro Loma Sanitary District.");
+    }
+    //CASANLEAN-1388
+
+} catch (err) {
+
+    logDebug(err)   }
+
+function isDistrictValid()
+{
+    var val = null;
+    var capParcelObj = cap.getParcelModel();
+    var parceMod = capParcelObj.getParcelModel();
+    var attArray = parceMod.getParcelAttribute().toArray();
+    for (att in attArray) {
+        if(attArray[att].getB1AttributeName() == "SEWERDISTRICT")
+            val = attArray[att].getB1AttributeValue()+"";
+    }
+
+    if( AInfo["Type of Work"] == "Sewer" && val && (val.toUpperCase().indexOf("SANITARY DISTRICT"))>-1 || (val.toUpperCase().indexOf("POLLUTION CONTROL PLANT"))>-1)
+        return true;
+    return false;
+}
