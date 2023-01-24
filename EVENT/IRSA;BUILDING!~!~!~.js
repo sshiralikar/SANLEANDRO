1822940120a5f9475529af9f025174f7615ec5e6
Shashank <sshiralikar@trustvip.com>
2023-01-24 17:58:08 -0500

    CASANLEAN-1499

--- EVENT/IRSA;BUILDING!~!~!~.js
+++ EVENT/IRSA;BUILDING!~!~!~.js
@@ -37,6 +37,45 @@ if(inspType == "2030 Final Electrical" &&
     }
 }
 
+
+//CASANLEAN-1499
+var params = aa.util.newHashtable();
+var applicantEmail = "";
+var conName = "";
+var contactResult = aa.people.getCapContactByCapID(capId);
+if (contactResult.getSuccess()) {
+    var capContacts = contactResult.getOutput();
+    for (var i in capContacts) {
+
+        var conName = getContactName(capContacts[i]);
+        var applicantEmail = capContacts[i].getPeople().getEmail()+"";
+        var inspectorName = getInspectorName(inspId);
+        if(!inspectorName)
+            inspectorName = "Inspector";
+        addParameter(params, "$$InspectorOfRecord$$", inspectorName);
+        addParameter(params, "$$altId$$", capId.getCustomID()+"");
+        addParameter(params, "$$InspectionStatus$$", inspResult);
+        addParameter(params, "$$FullNameBusName$$", conName);
+        addParameter(params, "$$InspectionType$$", inspType);
+        addParameter(params, "$$InspectionResultComment$$", inspComment);
+        sendEmail("no-reply@sanleandro.org", applicantEmail, "", "BLD_INSPECTION_RESULT_EMAIL", params, null, capId);
+
+    }
+}
+
+//CASANLEAN-1499
+
+function getInspectorName(pInspId) {
+    var inspResultObj = aa.inspection.getInspection(capId, pInspId);
+    if (inspResultObj.getSuccess()) {
+        iObj = inspResultObj.getOutput();
+        // have to re-grab the user since the id won't show up in this object.
+        inspUserObj = aa.person.getUser(iObj.getInspector().getFirstName(), iObj.getInspector().getMiddleName(), iObj.getInspector().getLastName()).getOutput();
+        return iObj.getInspector().getFirstName()  +" " + iObj.getInspector().getLastName();
+        //return inspUserObj.getUserID();
+    }
+    return false;
+}
 function isAllConditionsMet(vCapId)
 {
     var condResult = aa.capCondition.getCapConditions(vCapId);
@@ -55,4 +94,56 @@ function isAllConditionsMet(vCapId)
         }
     }
     return true;
+}
+function runEmailThroughSLEmailFilter(vEmail)
+{
+    var filter = lookup("SL_EMAIL_CONTROL", "FILTER");
+    if(filter == "ON")
+    {
+        var domains = String(lookup("SL_EMAIL_CONTROL", "DOMAIN_EXCEPTIONS"));
+        var emails = String(lookup("SL_EMAIL_CONTROL", "EMAIL_EXCEPTIONS"));
+        var vOriginalDomain = vEmail.substring(vEmail.indexOf("@") + 1, vEmail.length).toLowerCase();
+
+        if(domains.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+        if(emails.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+
+
+        vEmail = vEmail.replace(vOriginalDomain, "DoNotSend.com");
+    }
+    return vEmail;
+}
+function sendEmail(fromEmail, toEmail, CC, template, eParams, files) { // optional: itemCap
+    var itemCap = capId;
+    if (arguments.length == 7)
+        itemCap = arguments[6]; // use cap ID specified in args
+
+    //var sent = aa.document.sendEmailByTemplateName(fromEmail, toEmail, CC, template, eParams, files);
+    toEmail = runEmailThroughSLEmailFilter(toEmail);
+    var itempAltIDScriptModel = aa.cap.createCapIDScriptModel(itemCap.getID1(), itemCap.getID2(), itemCap.getID3());
+    var sent = aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, CC, template, eParams, itempAltIDScriptModel, files);
+    if (!sent.getSuccess()) {
+        logDebug("**WARN sending email failed, error:" + sent.getErrorMessage());
+    }
+}
+function getContactName(vConObj) {
+    if (vConObj.people.getContactTypeFlag() == "organization") {
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+    else {
+        if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "") {
+            return vConObj.people.getFullName();
+        }
+        if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null) {
+            return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
+        }
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
 }
