c9fe0ad0d474aa4f9f8b40ba0fcc079a5f34a3e4
sal-grayquarter <sal@grayquarter.com>
2023-01-25 10:53:01 -0800

    CASANLEAN-1542

--- EVENT/ASB;BUILDING!~!~!~.js
+++ EVENT/ASB;BUILDING!~!~!~.js
@@ -1,22 +1,24 @@
 //CASANLEAN-1519
 var lpList = aa.env.getValue("LicProfList");
-lpList = lpList.toArray();
-logDebug("LPs: " + lpList.length);
-var errors = [];
-for(var lpIndex in lpList) {
-    var lpObj = lpList[lpIndex];
-    var lpType = lpObj.licenseType;
-    var licenseNumber = lpObj.licenseNbr;
-    if(lpType == "Contractor") {
-        var cslbObj = validateCSLBClassifications(licenseNumber, appTypeString);
-        if(!cslbObj.validated) {
-            errors.push("<font size=small color=red>" + cslbObj.message + "</font>");
-        }
-    }    
-}
-if(errors.length > 0) {
-    showMessage = true;
-    comment(errors.join("<br>"));
-    cancel = true;
-    logDebug(errors.join("<br>"));
+if(lpList) {
+    lpList = lpList.toArray();
+    logDebug("LPs: " + lpList.length);
+    var errors = [];
+    for(var lpIndex in lpList) {
+        var lpObj = lpList[lpIndex];
+        var lpType = lpObj.licenseType;
+        var licenseNumber = lpObj.licenseNbr;
+        if(lpType == "Contractor") {
+            var cslbObj = validateCSLBClassifications(licenseNumber, appTypeString);
+            if(!cslbObj.validated) {
+                errors.push("<font size=small color=red>" + cslbObj.message + "</font>");
+            }
+        }    
+    }
+    if(errors.length > 0) {
+        showMessage = true;
+        comment(errors.join("<br>"));
+        cancel = true;
+        logDebug(errors.join("<br>"));
+    }
 }
