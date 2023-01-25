428d16eb1f7d815defef0ba3e3e9740fca7e565a
Shashank <sshiralikar@trustvip.com>
2023-01-25 09:38:49 -0500

    CASANLEAN-1446

--- EVENT/IRSA;BUILDING!~!~!~.js
+++ EVENT/IRSA;BUILDING!~!~!~.js
@@ -136,8 +136,8 @@ function getInspectorName(pInspId) {
     var inspResultObj = aa.inspection.getInspection(capId, pInspId);
     if (inspResultObj.getSuccess()) {
         iObj = inspResultObj.getOutput();
-        inspUserObj = aa.person.getUser(iObj.getInspector().getFirstName(), iObj.getInspector().getMiddleName(), iObj.getInspector().getLastName()).getOutput();
-        return iObj.getInspector().getFirstName()  +" " + iObj.getInspector().getLastName();
+        inspUserObj = aa.person.getUser(currentUserID).getOutput();
+        return inspUserObj.getFirstName()  +" " + inspUserObj.getLastName();
     }
     return false;
 }
