79d2c888ba8967df49582e5559fbb563d9d186aa
sal-grayquarter <sal@grayquarter.com>
2023-01-25 02:04:36 -0800

    CASANLEAN-1519

--- EVENT/ASB;BUILDING!~!~!~.js
+++ EVENT/ASB;BUILDING!~!~!~.js
@@ -1,9 +1,21 @@
-// Enter your script here...
-if(CAENumber > 0 && (CAELienseType == "Contractor")) {
-    var cslbObj = validateCSLBClassifications(CAENumber, appTypeString);
-    if(!cslbObj.validated) {
-        cancel = true;
-        showMessage = true;
-        message =  "<font size=small color=red>" + cslbObj.message + "</font>";
-    }
+//CASANLEAN-1519
+var lpList = aa.env.getValue("LicProfList");
+lpList = lpList.toArray();
+logDebug("LPs: " + lpList.length);
+var errors = [];
+for(var lpIndex in lpList) {
+    var lpObj = lpList[lpIndex];
+    var lpType = lpObj.licenseType;
+    var licenseNumber = lpObj.licenseNbr;
+    if(lpType == "Contractor") {
+        var cslbObj = validateCSLBClassifications(licenseNumber, appTypeString);
+        if(!cslbObj.validated) {
+            errors.push("<font size=small color=red>" + cslbObj.message + "</font>");
+        }
+    }    
+}
+if(errors.length > 0) {
+    cancel = true;
+    showMessage = true;
+    message += errors.join("<br>");
 }
