4b13c4bca4950466014aad712c06b736a3c3d5a2
Shashank <sshiralikar@trustvip.com>
2023-01-25 09:53:48 -0500

    CASANLEAN-1537

--- EVENT/IRSA;BUILDING!~!~!~.js
+++ EVENT/IRSA;BUILDING!~!~!~.js
@@ -131,6 +131,43 @@ if (contactResult.getSuccess()) {
 
 //CASANLEAN-1499
 
+
+//CASANLEAN-1537
+if(appMatch("Building/Combo/NA/NA") && (inspType =="3000 Final - Building Permit")||
+    appMatch("Building/Commercial/Accessory/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Commercial/Addition/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Commercial/Alteration/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Commercial/Cell/NA") && (inspType =="2030 Final Electrical")||
+    appMatch("Building/Commercial/Demolition/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Commercial/Electric/NA") && (inspType =="2030 Final Electrical")||
+    appMatch("Building/Commercial/Mechanic/NA") && (inspType =="2020 Final Mechanical")||
+    appMatch("Building/Commercial/New Construction/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Commercial/Plumbing/NA") && (inspType =="2010 Final Plumbing")||
+    appMatch("Building/Commercial/Pool/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Commercial/Roofing/NA") && (inspType =="1540 Final Re-Roof")||
+    appMatch("Building/Commercial/Sign/NA") && (inspType =="1930 Final Sign")||
+    appMatch("Building/Commercial/Solar/NA") && (inspType =="2030 Final Electrical")||
+    appMatch("Building/Residential/Accessory/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Residential/Addition/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Residential/ADU/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Residential/Alteration/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Residential/Demolition/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Residential/Electric/NA") && (inspType =="2030 Final Electrical")||
+    appMatch("Building/Residential/Mechanical/NA") && (inspType =="2020 Final Mechanical")||
+    appMatch("Building/Residential/New Construction/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Residential/Plumbing/NA") && (inspType =="2010 Final Plumbing")||
+    appMatch("Building/Residential/Pool/NA") && (inspType =="3000 Final Building Permit")||
+    appMatch("Building/Residential/Roofing/NA") && (inspType =="1540 Final Re-Roof")||
+    appMatch("Building/Residential/Solar/NA") && (inspType =="2030 Final Electrical")
+    && inspResult == "Pass" && vBalanceDue <= 0)
+{
+    updateTask("Inspection"," Final Inspection Complete","","");
+    aa.workflow.adjustTask(capId, "Inspection", "N", "Y", null, null);
+    updateAppStatus("Finaled","Updated through script");
+}
+//CASANLEAN-1537
+
+
 function getInspectorName(pInspId) {
     var inspResultObj = aa.inspection.getInspection(capId, pInspId);
     if (inspResultObj.getSuccess()) {
