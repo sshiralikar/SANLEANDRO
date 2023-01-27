8b19bdeafe2e80a27abc19739692c521460a2e8b
Shashank <sshiralikar@trustvip.com>
2023-01-27 15:53:44 -0500

    FIC remove

--- EVENT/IRSA;BUILDING!~!~!~.js
+++ EVENT/IRSA;BUILDING!~!~!~.js
@@ -177,7 +177,7 @@ if(appMatch("Building/Combo/NA/NA") && (inspType =="3000 Final - Building Permit
     {
         updateTask("Inspection","Final Inspection Complete","","");
         aa.workflow.adjustTask(capId, "Inspection", "N", "Y", null, null);
-        updateAppStatus("Final Inspection Complete","Updated through script");
+        //updateAppStatus("Final Inspection Complete","Updated through script");
         addStdCondition("General", "Balance Due", capId);
     }
 }
