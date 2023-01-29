ba8d9c51d7f0081e8426574214fa0f6865ba79f2
Shashank <sshiralikar@trustvip.com>
2023-01-29 12:15:08 -0500

    //CASANLEAN-1504

--- EVENT/ISB;BUILDING!~!~!~.js
+++ EVENT/ISB;BUILDING!~!~!~.js
@@ -41,3 +41,27 @@ if(((appMatch("Building/Combo/NA/NA") && (inspType =="3000 Final - Building Perm
     showMessage = true;
     comment("Balance Due, Final inspection cannot be scheduled.");
 }
+//CASANLEAN-1504
+if(getAppStatus(capId) != "Ready for Inspection")
+{
+    cancel = true;
+    showMessage = true;
+    comment("Inspections can only be scheduled when the permit is in the status: <b>'Ready for Inspection'</b>.");
+}
+//CASANLEAN-1504
+function getAppStatus() {
+    var itemCap = capId;
+    if (arguments.length == 1) itemCap = arguments[0]; // use cap ID specified in args
+
+    var appStatus = null;
+    var capResult = aa.cap.getCap(itemCap);
+    if (capResult.getSuccess()) {
+        licCap = capResult.getOutput();
+        if (licCap != null) {
+            appStatus = "" + licCap.getCapStatus();
+        }
+    } else {
+        logDebug("ERROR: Failed to get app status: " + capResult.getErrorMessage());
+    }
+    return appStatus;
+}
