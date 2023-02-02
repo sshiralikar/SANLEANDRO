f0c572472b1bfa36c1bcfbf333107e11c194e2fe
Shashank <sshiralikar@trustvip.com>
2023-02-02 13:32:02 -0500

    CASANLEAN-1554

--- EVENT/ISB;BUILDING!~!~!~.js
+++ EVENT/ISB;BUILDING!~!~!~.js
@@ -49,6 +49,46 @@ if(publicUser && getAppStatus(capId) != "Ready for Inspection")
     comment("Inspections can only be scheduled when the permit is in the status: <b>'Ready for Inspection'</b>.");
 }
 //CASANLEAN-1504
+
+//CASANLEAN-1554
+if(!publicUser && inspType == "2050 Electrical Service Release")
+{
+    var fields = "";
+    if(AInfo["Service Status"] == null || AInfo["Service Status"] == "")
+        fields +="<br>  "+"Service Status";
+    if(AInfo["Supply Service"] == null || AInfo["Supply Service"] == "")
+        fields +="<br>  "+"Supply Service";
+    if(AInfo["Meter Socket"] == null || AInfo["Meter Socket"] == "")
+        fields +="<br>  "+"Meter Socket";
+    if(AInfo["Amps"] == null || AInfo["Amps"] == "")
+        fields +="<br>  "+"Amps";
+    if(fields!="")
+    {
+        cancel = true;
+        showMessage = true;
+        comment("Please fill out the following fields: "+ fields);
+    }
+}
+
+if(!publicUser && inspType == "2060 Gas Service Release")
+{
+    var fields = "";
+    if(AInfo["Service Status"] == null || AInfo["Service Status"] == "")
+        fields +="<br>  "+"Service Status";
+    if(AInfo["Supply Service"] == null || AInfo["Supply Service"] == "")
+        fields +="<br>  "+"Supply Service";
+    if(AInfo["BTU"] == null || AInfo["BTU"] == "")
+        fields +="<br>  "+"Amps";
+    if(fields!="")
+    {
+        cancel = true;
+        showMessage = true;
+        comment("Please fill out the following fields: "+ fields);
+    }
+}
+//CASANLEAN-1554
+
+
 function getAppStatus() {
     var itemCap = capId;
     if (arguments.length == 1) itemCap = arguments[0]; // use cap ID specified in args
