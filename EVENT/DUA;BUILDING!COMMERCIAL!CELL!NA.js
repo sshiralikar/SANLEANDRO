164c8dec40bf0c45b81da54c88133c71ba028063
Shashank <sshiralikar@trustvip.com>
2023-03-02 13:42:22 -0500

    CASANLEAN-2585

--- EVENT/DUA;BUILDING!COMMERCIAL!CELL!NA.js
+++ EVENT/DUA;BUILDING!COMMERCIAL!CELL!NA.js
@@ -4,19 +4,22 @@ if (workfHistory.getSuccess()) {
 }
 for (var i in wfhistoryresult) {
     var pTask = wfhistoryresult[i];
-    if (isTaskActive("Plans Distribution") && pTask.getTaskDescription() == "Plans Distribution"
+    if (pTask.getTaskDescription() == "Plans Distribution"
         && pTask.getDisposition() == "Resubmittal Required") {
         updateTask("Plans Distribution","Updated Documents","","");
+        updateAppStatus("In Review","Updated through script");
         break;
     }
     if (isTaskActive("Fire Plan Review") && pTask.getTaskDescription() == "Fire Plan Review"
         && pTask.getDisposition() == "Resubmittal Required") {
         updateTask("Fire Plan Review","Updated Documents","","");
+        updateAppStatus("In Review","Updated through script");
         break;
     }
-    if (isTaskActive("Plans Coordination") && pTask.getTaskDescription() == "Plans Coordination"
+    if (isTaskActive("Plans Distribution") && pTask.getTaskDescription() == "Plans Coordination"
         && pTask.getDisposition() == "Resubmittal Required") {
-        updateTask("Plans Coordination","Updated Documents","","");
+        updateTask("Plans Distribution","Updated Documents","","");
+        updateAppStatus("In Review","Updated through script");
         break;
     }
 }
--- EVENT/DUA;BUILDING!COMMERCIAL!MECHANICAL!NA.js
+++ EVENT/DUA;BUILDING!COMMERCIAL!MECHANICAL!NA.js
@@ -1,25 +1,25 @@
-//CASANLEAN-473
 var workfHistory = aa.workflow.getWorkflowHistory(capId, null);
 if (workfHistory.getSuccess()) {
     var wfhistoryresult = workfHistory.getOutput();
 }
 for (var i in wfhistoryresult) {
     var pTask = wfhistoryresult[i];
-    if (isTaskActive("Plans Distribution") && pTask.getTaskDescription() == "Plans Distribution"
+    if (pTask.getTaskDescription() == "Plans Distribution"
         && pTask.getDisposition() == "Resubmittal Required") {
         updateTask("Plans Distribution","Updated Documents","","");
+        updateAppStatus("In Review","Updated through script");
         break;
     }
     if (isTaskActive("Fire Plan Review") && pTask.getTaskDescription() == "Fire Plan Review"
         && pTask.getDisposition() == "Resubmittal Required") {
         updateTask("Fire Plan Review","Updated Documents","","");
+        updateAppStatus("In Review","Updated through script");
         break;
     }
-    if (isTaskActive("Plans Coordination") && pTask.getTaskDescription() == "Plans Coordination"
+    if (isTaskActive("Plans Distribution") && pTask.getTaskDescription() == "Plans Coordination"
         && pTask.getDisposition() == "Resubmittal Required") {
-        updateTask("Plans Coordination","Updated Documents","","");
+        updateTask("Plans Distribution","Updated Documents","","");
+        updateAppStatus("In Review","Updated through script");
         break;
     }
 }
-
-//CASANLEAN-473
