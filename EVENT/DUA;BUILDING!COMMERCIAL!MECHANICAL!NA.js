adc94b987e11db5aba5bf4995aa3e47422461f5d
Shashank <sshiralikar@trustvip.com>
2023-03-02 12:52:01 -0500

    CASANLEAN-2585

--- EVENT/DUA;BUILDING!COMMERCIAL!MECHANICAL!NA.js
+++ EVENT/DUA;BUILDING!COMMERCIAL!MECHANICAL!NA.js
@@ -5,10 +5,21 @@ if (workfHistory.getSuccess()) {
 }
 for (var i in wfhistoryresult) {
     var pTask = wfhistoryresult[i];
-    if (pTask.getTaskDescription() == "Plans Distribution"
+    if (isTaskActive("Plans Distribution") && pTask.getTaskDescription() == "Plans Distribution"
         && pTask.getDisposition() == "Resubmittal Required") {
         updateTask("Plans Distribution","Updated Documents","","");
         break;
     }
+    if (isTaskActive("Fire Plan Review") && pTask.getTaskDescription() == "Fire Plan Review"
+        && pTask.getDisposition() == "Resubmittal Required") {
+        updateTask("Fire Plan Review","Updated Documents","","");
+        break;
+    }
+    if (isTaskActive("Plans Coordination") && pTask.getTaskDescription() == "Plans Coordination"
+        && pTask.getDisposition() == "Resubmittal Required") {
+        updateTask("Plans Coordination","Updated Documents","","");
+        break;
+    }
 }
+
 //CASANLEAN-473
