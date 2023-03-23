f664c96c6a9e8df05ebf14c21c221c571389438f
Shashank <sshiralikar@trustvip.com>
2023-03-23 11:36:05 -0400

    CASANLEAN-2610

--- EVENT/DUA;BUILDING!~!~!~.js
+++ EVENT/DUA;BUILDING!~!~!~.js
@@ -1,1 +1,33 @@
 loadCustomScript("DUA_EXECUTE_DIGEPLAN_SCRIPTS");
+if (isTaskStatusActive("Plans Coordination", "Resubmittal Required")) {
+    updateTask("Plans Coordination","In Progress","","");
+}
+if (isTaskStatusActive("Plans Distribution", "Resubmittal Required")) {
+    updateTask("Plans Distribution","In Progress","","");
+}
+function isTaskStatusActive(wfstr,status) // optional process name
+{
+    var useProcess = false;
+    var processName = "";
+    if (arguments.length == 2)
+    {
+        processName = arguments[1]; // subprocess
+        useProcess = true;
+    }
+
+    var workflowResult = aa.workflow.getTasks(capId);
+    if (workflowResult.getSuccess())
+        wfObj = workflowResult.getOutput();
+    else
+    { logMessage("**ERROR: Failed to get workflow object: " + s_capResult.getErrorMessage()); return false; }
+
+    for (i in wfObj)
+    {
+        fTask = wfObj[i];
+        if (fTask.getTaskDescription().toUpperCase().equals(wfstr.toUpperCase())  && (!useProcess || fTask.getProcessCode().equals(processName)))
+            if (fTask.getActiveFlag().equals("Y") && fTask.getDisposition() == status)
+                return true;
+            else
+                return false;
+    }
+}
