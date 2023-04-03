8adcc7dbb3203c9cbe46b6481f2d564c5c65ba13
sal-grayquarter <sal@grayquarter.com>
2023-04-03 16:54:42 -0700

    CASANLEAN-2646 undefined message

--- EVENT/WTUB;BUILDING!~!~!~.js
+++ EVENT/WTUB;BUILDING!~!~!~.js
@@ -7,7 +7,7 @@ if(wfStatus == "Issued" && !appHasCondition("General", "Met", "CSLB Expired", nu
         comment(expiredLPData.join("<br>"));
         aa.env.setValue("capId", String(capId.getCustomID()));
         var result = aa.runScriptInNewTransaction("BLD_APPLY_CSLB_CONDITION");
-        comment(result);
+        //comment(result);
     }
 } else {
     logDebug("Condition for CSLB Expired met...");
