2cd52d55d7f2cf298873908afd34168b717bc6db
sal-grayquarter <sal@grayquarter.com>
2023-04-03 13:34:54 -0700

    CASANLEAN-2646

--- EVENT/WTUB;BUILDING!~!~!~.js
+++ EVENT/WTUB;BUILDING!~!~!~.js
@@ -1,7 +1,12 @@
-//CASANLEAN-1542 - Validates at every step of workflow
-var expiredLPData = validateFromCSLB(null, capId, appTypeString);
-if(expiredLPData && expiredLPData.length > 0) {
-    cancel = true;
-    showMessage = true;
-    comment(expiredLPData.join("<br>"));
+//CASANLEAN-1542, CASANLEAN-2646 - Validates issuance workflow
+if(wfStatus == "Issued" && !appHasCondition("General", "Met", "CSLB Expired", null)) {
+    var expiredLPData = validateFromCSLB(null, capId, appTypeString);
+    if(expiredLPData && expiredLPData.length > 0) {
+        cancel = true;
+        showMessage = true;
+        comment(expiredLPData.join("<br>"));
+        addStdCondition("General", "CSLB Expired", capId);
+    }
+} else {
+    logDebug("Condition for CSLB Expired met...");
 }
