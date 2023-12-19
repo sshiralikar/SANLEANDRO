798dc5d85733ba809107e1b88d94fcb9f421ff89
sal-grayquarter <sal@grayquarter.com>
2023-12-19 15:16:33 -0800

    ENV sync

--- EVENT/WTUA;ENGINEERING!~!~!~.js
+++ EVENT/WTUA;ENGINEERING!~!~!~.js
@@ -1,3 +1,171 @@
+// WTUA ENGINEERING
+//
+vCurrentUser=currentUserID;
+if(wfTask == "Application Intake")
+{
+    //assignTask("Application Intake",currentUserID);
+    assignCap(currentUserID);
+}
+if(wfTask == "Application Intake" && (wfStatus == "Accepted - Plan Review Req" || wfStatus == "Additional Info Required" || wfStatus == "In Progress")) {
+//	var recAsgnStaff = capDetail.getAsgnStaff();
+	logDebug('What is recAsgnStaff = ' + vCurrentUser);
+	if (vCurrentUser != null) {
+		wfTkArray = new Array();
+		wfTkArray = loadTasks(capId);
+		//added Review task to list - user story3465
+		for (x in wfTkArray) {
+			if (matches(x, 'Application Intake', 'Plans Distribution','Engineering Review','Plans Coordination','Permit Issuance')) {
+				assignTask(x, vCurrentUser);
+			}
+		}
+	}
+}
+
+
+
+//CASANLAN - 2931
+
+    var params = aa.util.newHashtable();
+
+    var vStaffId = "";
+    var title = "";
+    var vEmail = "";
+    var wfUserName = "";
+    var currentUsrVar = aa.person.getUser(wfStaffUserID).getOutput();
+    if (currentUsrVar != null) {
+        vStaffId = currentUsrVar.getGaUserID();
+        title = currentUsrVar.title;
+        vEmail = currentUsrVar.email;
+        wfUserName = currentUsrVar.firstName + " "+ currentUsrVar.middleName+ " "+ currentUsrVar.lastName;
+    }
+
+    var applicantEmail = "";
+    var conName = "";
+    var contactResult = aa.people.getCapContactByCapID(capId);
+    if (contactResult.getSuccess()) {
+        var capContacts = contactResult.getOutput();
+        for (var i in capContacts) {
+            if (capContacts[i].getPeople().getContactType() == "Applicant") {
+                conName = getContactName(capContacts[i]);
+                applicantEmail = capContacts[i].getPeople().getEmail()+"";
+            }
+        }
+    }
+    addParameter(params, "$$applicantName$$", conName);
+    addParameter(params, "$$altID$$", capId.getCustomID()+"");
+    addParameter(params, "$$assignedToStaff$$", wfUserName);
+    addParameter(params, "$$assignedUserEmail$$", vEmail);
+    addParameter(params, "$$wfTaskComments$$", wfComment);
+    addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+    
+
+if(wfTask == "Application Intake" && wfStatus == "Additional Info Required"){
+	sendEmail("no-reply@sanleandro.org", applicantEmail, "", "ENG_ADDITIONAL_INFO_REQ", params, null, capId);
+}
+
+//CASANLAN - 2942
+if(wfTask == "Engineering Review" && wfStatus == "Revisions Required"){
+   sendEmail("no-reply@sanleandro.org", applicantEmail, "", "ENG_REVISIONS_REQUIRED", params, null, capId);
+}
+
+//CASANLAN - 2933
+if(wfTask == "Traffic Review" && wfStatus == "Revisions/Resubmittal Required"){
+   sendEmail("no-reply@sanleandro.org", applicantEmail, "", "ENG_TRAFFIC_RESUBMITTAL_REQUIRED", params, null, capId);
+}
+
+//CASANLAN - 2937
+if(wfTask == "Permit Issuance" && wfStatus == "Issue"){
+   sendEmail("no-reply@sanleandro.org", applicantEmail, "", "ENG_PERMIT_ISSUE", params, null, capId);
+}
+
+//CASANLAN - ????
+if(wfTask == "Plans Coordination" && wfStatus == "Resubmittal Required"){
+    sendEmail("no-reply@sanleandro.org", applicantEmail, "", "ENG_PLANS_COORDINATION_RESUBMITTAL_REQUIRED", params, null, capId);
+}
+
+//CASANLAN - 2931
+if(wfTask == "Plans Coordination" && wfStatus == "Approved Fees Due"){
+    sendEmail("no-reply@sanleandro.org", applicantEmail, "", "ENG_APPROVED_FEES_DUE", params, null, capId);
+}
+
+//CASANLAN - 2960
+if(wfTask == "Plans Coordination" && wfStatus == "Fees Paid"){
+	
+// get assigned user on record 
+capDetail = aa.cap.getCapDetail(capId).getOutput();
+
+userObj = aa.person.getUser(capDetail.getAsgnStaff());
+
+if (userObj.getSuccess()) {
+    staff = userObj.getOutput();
+    userID = staff.getUserID();
+    var userObj = aa.person.getUser(userID);
+    if (userObj.getSuccess()) {
+		var userInfo = userObj.getOutput();
+        userEmail = userInfo.getEmail()+"";
+        logDebug("userEmail: " + userEmail)
+    }
+}
+    sendEmail("no-reply@sanleandro.org", userEmail, "", "ENG_PERMIT_ISSUE", params, null, capId);
+}
+
+//CASANLAN - 2931
+function runEmailThroughSLEmailFilter(vEmail)
+{
+    var filter = lookup("SL_EMAIL_CONTROL", "FILTER");
+    if(filter == "ON")
+    {
+        var domains = String(lookup("SL_EMAIL_CONTROL", "DOMAIN_EXCEPTIONS"));
+        var emails = String(lookup("SL_EMAIL_CONTROL", "EMAIL_EXCEPTIONS"));
+        var vOriginalDomain = vEmail.substring(vEmail.indexOf("@") + 1, vEmail.length).toLowerCase();
+
+        if(domains.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+        if(emails.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+
+
+        vEmail = vEmail.replace(vOriginalDomain, "DoNotSend.com");
+    }
+    return vEmail;
+}
+
+
+function sendEmail(fromEmail, toEmail, CC, template, eParams, files) { // optional: itemCap
+    var itemCap = capId;
+    if (arguments.length == 7)
+        itemCap = arguments[6]; // use cap ID specified in args
+
+    //var sent = aa.document.sendEmailByTemplateName(fromEmail, toEmail, CC, template, eParams, files);
+    toEmail = runEmailThroughSLEmailFilter(toEmail);
+    var itempAltIDScriptModel = aa.cap.createCapIDScriptModel(itemCap.getID1(), itemCap.getID2(), itemCap.getID3());
+    var sent = aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, CC, template, eParams, itempAltIDScriptModel, files);
+    if (!sent.getSuccess()) {
+        logDebug("**WARN sending email failed, error:" + sent.getErrorMessage());
+    }
+}
+function getContactName(vConObj) {
+    if (vConObj.people.getContactTypeFlag() == "organization") {
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+    else {
+        if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "") {
+            return vConObj.people.getFullName();
+        }
+        if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null) {
+            return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
+        }
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+}
+
+//CASANLEAN-2978
 if(wfTask == "Plans Coordination" && wfStatus == "Hold for Signature") {
     var reportParams = aa.util.newHashtable();
     reportParams.put("RECORD_ID", String(capId.getCustomID()));
