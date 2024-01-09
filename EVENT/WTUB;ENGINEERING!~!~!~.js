ceee833f7892eca3990a0a092913807ca84d4d3b
sal-grayquarter <sal@grayquarter.com>
2024-01-08 22:32:32 -0800

    CASANLEAN-3004/CASANLEAN-3005 Notify CSLB and move to Adobe Sign trigger

--- EVENT/WTUB;ENGINEERING!~!~!~.js
+++ EVENT/WTUB;ENGINEERING!~!~!~.js
@@ -1,9 +1,10 @@
 //CASANLEAN-2983
-if(wfTask == "Permit Issuance" && wfStatus == "Issued") {
-    var expiredLPData = validateFromCSLB(null, capId, appTypeString);
+if(wfTask == "Plans Coordination" && wfStatus == "Hold for Signature") {
+    var expiredLPData = validateFromCSLBEng(null, capId, appTypeString);
     if(expiredLPData && expiredLPData.length > 0) {
         cancel = true;
         showMessage = true;
         comment(expiredLPData.join("<br>"));
     }
+    //Contractor CSLB Information Expired
 }
