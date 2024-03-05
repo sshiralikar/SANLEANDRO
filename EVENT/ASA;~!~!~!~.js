dca89a26d2d704e28e0178b7865db6d0e1b39cab
sal-grayquarter <sal@grayquarter.com>
2024-03-04 21:04:14 -0800

    env differences

--- EVENT/ASA;~!~!~!~.js
+++ EVENT/ASA;~!~!~!~.js
@@ -1,7 +1,7 @@
 //Application Submit After Event Entry Point
 showDebug = false;
 showMessage = false;
-if(matches(currentUserID,"KHOBDAY","MNICCORE","ADMIN","SSHIRALIKAR"))
+if(matches(currentUserID,"KHOBDAY","MNICCORE","ADMIN","SSHIRALIKAR","JDOLEZAL"))
     showDebug = 1;
 if(appMatch("Planning/Project/*/*"))
     include("ES_ENTITLEMENT_LOOP");
@@ -9,6 +9,9 @@ if(appMatch("Building/*/*/*"))
     include("ES_BLD_ASA");
 if(appMatch("Planning/*/*/*"))
     include("ES_PLN_ASA");
+if(appMatch("Engineering/*/*/*"))
+    include("ES_ENG_ASA");
+
 
 //#GIS Interface
 copyParcelGisObjects();
