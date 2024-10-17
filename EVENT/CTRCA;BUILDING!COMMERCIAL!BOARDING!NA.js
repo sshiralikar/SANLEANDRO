a161f1a5da9f0f12562d60e6becb7b1cf953fc6b
Shashank <110970408+sshiralikar@users.noreply.github.com>
2024-10-16 23:24:37 -0400

    Boarding Permit Scripts

---
+++ EVENT/CTRCA;BUILDING!COMMERCIAL!BOARDING!NA.js
@@ -0,0 +1,91 @@
+//CASANLEAN-1297
+var params = aa.util.newHashtable();
+
+var vAddress = "";
+var capAddressResult1 = aa.address.getAddressByCapId(capId);
+if (capAddressResult1.getSuccess())
+{
+    var Address = capAddressResult1.getOutput();
+    for (yy in Address)
+    {
+        vAddress = Address[yy].getHouseNumberStart();
+        if (Address[yy].getStreetDirection())
+            vAddress += " " + Address[yy].getStreetDirection();
+        vAddress += " " + Address[yy].getStreetName();
+        if (Address[yy].getStreetSuffix())
+            vAddress += " " + Address[yy].getStreetSuffix();
+        if (Address[yy].getUnitStart())
+            vAddress += " " + Address[yy].getUnitStart();
+    }
+}
+var applicantEmail = "";
+var conName = "";
+var contactResult = aa.people.getCapContactByCapID(capId);
+if (contactResult.getSuccess()) {
+    var capContacts = contactResult.getOutput();
+    for (var i in capContacts) {
+        if (capContacts[i].getPeople().getContactType() == "Applicant") {
+            conName = getContactName(capContacts[i]);
+            applicantEmail = capContacts[i].getPeople().getEmail()+"";
+        }
+    }
+}
+addParameter(params, "$$applicantName$$", conName);
+addParameter(params, "$$altID$$", capId.getCustomID()+"");
+addParameter(params, "$$projectDescription$$", cap.getSpecialText());
+addParameter(params, "$$Location$$", vAddress);
+addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+sendEmail("no-reply@sanleandro.org", applicantEmail, "", "BLD_ACA_APPLICATION_RECEIVED", params, null, capId);
+//CASANLEAN-124
+function runEmailThroughSLEmailFilter(vEmail)
+{
+    var filter = lookup("SL_EMAIL_CONTROL", "FILTER");
+    if(filter == "ON")
+    {
+        var domains = String(lookup("SL_EMAIL_CONTROL", "DOMAIN_EXCEPTIONS"));
+        var emails = String(lookup("SL_EMAIL_CONTROL", "EMAIL_EXCEPTIONS"));
+        var vOriginalDomain = vEmail.substring(vEmail.indexOf("@") + 1, vEmail.length).toLowerCase();
+
+        if(domains.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+        if(emails.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+
+
+        vEmail = vEmail.replace(vOriginalDomain, "DoNotSend.com");
+    }
+    return vEmail;
+}
+function sendEmail(fromEmail, toEmail, CC, template, eParams, files) { // optional: itemCap
+    var itemCap = capId;
+    if (arguments.length == 7)
+        itemCap = arguments[6]; // use cap ID specified in args
+
+    //var sent = aa.document.sendEmailByTemplateName(fromEmail, toEmail, CC, template, eParams, files);
+    toEmail = runEmailThroughSLEmailFilter(toEmail);
+    var itempAltIDScriptModel = aa.cap.createCapIDScriptModel(itemCap.getID1(), itemCap.getID2(), itemCap.getID3());
+    var sent = aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, CC, template, eParams, itempAltIDScriptModel, files);
+    if (!sent.getSuccess()) {
+        logDebug("**WARN sending email failed, error:" + sent.getErrorMessage());
+    }
+}
+function getContactName(vConObj) {
+    if (vConObj.people.getContactTypeFlag() == "organization") {
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+    else {
+        if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "") {
+            return vConObj.people.getFullName();
+        }
+        if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null) {
+            return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
+        }
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+}
--- EVENT/WTUB;BUILDING!~!~!~.js
+++ EVENT/WTUB;BUILDING!~!~!~.js
@@ -1,6 +1,6 @@
 //CASANLEAN-1542, CASANLEAN-2646 - Validates issuance workflow
 if(wfStatus == "Issued" && !appHasCondition("General", "Met", "CSLB Expired", null)) {
-    var expiredLPData = validateFromCSLB(null, capId, appTypeString);
+    var expiredLPData = validateFromCSLBEng(null, capId, appTypeString);
     if(expiredLPData && expiredLPData.length > 0) {
         cancel = true;
         showMessage = true;
